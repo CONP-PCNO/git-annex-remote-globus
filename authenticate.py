9ea54141dc1888f8b438d967faa4d41244adac42
gi114 <giulia.ippoliti@mail.mcgill.ca>
2019-10-10 10:36:28 -0400

    download file via wget

--- authenticate.py
+++ authenticate.py
@@ -1,5 +1,12 @@
 import globus_sdk
+import os.path as op
+import wget
+import globus_sdk.base as base
 import json
+from globus_sdk import LocalGlobusConnectPersonal
+
+# LOCAL GLOBUS CONNECT PERSONAL
+#  https://github.com/globus/globus-sdk-python
 
 
 def get_endpoint_id():
@@ -8,9 +15,15 @@ def get_endpoint_id():
         return ep['id']
 
 
+def get_my_endpoint_id():
+    for ep in tc.endpoint_search(filter_fulltext='my-endpoints', num_results=None):
+        print('{0} has ID {1}'.format(ep['display_name'], ep['id']))
+        return ep['id']
+
+
 def get_path_content(ep_id):
     print("In get path content - ls")
-    for en in tc.operation_ls(ep_id, path="/~/5/published/publication_170/submitted_data/"):
+    for en in tc.operation_ls(ep_id, path="/~/5/published/publication_170/submitted_data/2015_11_18_cortex/"):
         print(en)
 
 
@@ -46,24 +59,47 @@ print(TRANSFER_TOKEN)
 authorizer = globus_sdk.AccessTokenAuthorizer(TRANSFER_TOKEN)
 tc = globus_sdk.TransferClient(authorizer=authorizer)
 
-# CREATE AN ENDPOINT
-ep_data = {
-    "DATA_TYPE": "endpoint",
-    "display_name": 'test',
-    "DATA": [
-        {
-            "DATA_TYPE": "server",
-            "hostname": "gridftp.globus.org",
-        },
-    ],
-}
-create_result = tc.create_endpoint(ep_data)
+# # CREATE AN ENDPOINT
+# ep_data = {
+#     "DATA_TYPE": "endpoint",
+#     "display_name": 'personal',
+#     "DATA": [
+#         {
+#             "DATA_TYPE": "personal",
+#         },
+#     ],
+# }
+# create_result = tc.create_endpoint(ep_data)
 
 # GET THE FRDR-Prod-2 ID (This is what we need)
 
 frdr_id = get_endpoint_id()
 get_path_content(frdr_id)
+endpoint = tc.get_endpoint(frdr_id)
 
+# DATA LOCATION LINK
+# base: https://app.globus.org/file-manager?origin_id=8ca92f91-39fb-4176-bcb9-7fb1ed53114b&origin_path=%2F
+# location: https://app.globus.org/file-manager?origin_id=8ca92f91-39fb-4176-bcb9-7fb1ed53114b&origin_path=%2F5%2Fpublished%2Fpublication_170%2Fsubmitted_data%2F
+
+https_server = endpoint['https_server']
+print("HTTPS SERVER:", https_server)
+
+source_path = "/5/published/publication_170/submitted_data/2015_11_18_cortex/2015_11_18_cortex.json"
+
+# create download link by making path: https_server + source_path
+download_path = op.join(https_server, source_path)
+print(download_path)
+url='https://2a9f4.8443.dn.glob.us/5/published/publication_170/submitted_data/2015_11_18_cortex/2015_11_18_cortex.json'
+wget.download(url=url)
+# label = "My json transfer"
+#
+# # TransferData() automatically gets a submission_id for once-and-only-once submission
+# tdata = globus_sdk.TransferData(tc, source_endpoint_id,
+#                                 dest_endpoint_id,
+#                                 label=label)
+#
+# # Recursively transfer source path contents
+# tdata.add_item(source_path, dest_path, recursive=True)
 
 # IMPORTANT PAGES: https://docs.globus.org/globus-connect-server-installation-guide/
 # https://docs.globus.org/globus-connect-server-v5-installation-guide/#_creating_a_storage_gateway_using_the_posix_storage_connector
@@ -96,3 +132,12 @@ get_path_content(frdr_id)
 
 
 # TRY AND NAVIGATE IN THE ENDPOINT PATH AND RETRIVE DATA
+
+# HOW TO TRANSFER DATA (GLOBUS PRINCIPLE)
+# How can I transfer files to and from my laptop or desktop?
+#
+# Use Globus Connect Personal, which provides a point-and-click interface for configuring and operating a Globus endpoint
+# on your local machine. After installing Globus Connect Personal, your computer looks just like any other Globus endpoint
+# so all the standard Globus web and command line interface features will work. Globus Connect Personal runs behind NATs
+# and firewalls, as long as it can make an outbound connection. Click the link for your operating system to get started
+# with Globus Connect Personal: Mac OS X, Windows, Linux.
