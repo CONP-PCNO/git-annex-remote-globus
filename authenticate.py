31fff8c0745f33574c8b06554bd2d0fd52cb3b12
gi114 <giulia.ippoliti@mail.mcgill.ca>
2019-10-11 15:40:20 -0400

    list all files in dataset

--- authenticate.py
+++ authenticate.py
@@ -15,16 +15,13 @@ def get_endpoint_id():
         return ep['id']
 
 
-def get_my_endpoint_id():
-    for ep in tc.endpoint_search(filter_fulltext='my-endpoints', num_results=None):
-        print('{0} has ID {1}'.format(ep['display_name'], ep['id']))
-        return ep['id']
-
-
-def get_path_content(ep_id):
-    print("In get path content - ls")
-    for en in tc.operation_ls(ep_id, path="/~/5/published/publication_170/submitted_data/2015_11_18_cortex/"):
-        print(en)
+def get_path_content(ep_id, path):
+    for en in tc.operation_ls(ep_id, path=path, num_results=None):
+        new_path = op.join(path, en['name'])
+        if en['type'] == 'file':
+            print(new_path)
+        else:
+            get_path_content(ep_id, new_path)
 
 
 CLIENT_ID = '01589ab6-70d1-4e1c-b33d-14b6af4a16be'
@@ -59,6 +56,15 @@ print(TRANSFER_TOKEN)
 authorizer = globus_sdk.AccessTokenAuthorizer(TRANSFER_TOKEN)
 tc = globus_sdk.TransferClient(authorizer=authorizer)
 
+# GET THE FRDR-Prod-2 ID (This is what we need)
+
+frdr_id = get_endpoint_id()
+
+url1 = 'https://2a9f4.8443.dn.glob.us/5/published/publication_170/submitted_data/'
+base_path = '/~/5/published/publication_170/submitted_data/'
+
+get_path_content(frdr_id, base_path)
+
 # # CREATE AN ENDPOINT
 # ep_data = {
 #     "DATA_TYPE": "endpoint",
@@ -71,26 +77,21 @@ tc = globus_sdk.TransferClient(authorizer=authorizer)
 # }
 # create_result = tc.create_endpoint(ep_data)
 
-# GET THE FRDR-Prod-2 ID (This is what we need)
 
-frdr_id = get_endpoint_id()
-get_path_content(frdr_id)
-endpoint = tc.get_endpoint(frdr_id)
+
+# endpoint = tc.get_endpoint(frdr_id)
 
 # DATA LOCATION LINK
 # base: https://app.globus.org/file-manager?origin_id=8ca92f91-39fb-4176-bcb9-7fb1ed53114b&origin_path=%2F
 # location: https://app.globus.org/file-manager?origin_id=8ca92f91-39fb-4176-bcb9-7fb1ed53114b&origin_path=%2F5%2Fpublished%2Fpublication_170%2Fsubmitted_data%2F
 
-https_server = endpoint['https_server']
-print("HTTPS SERVER:", https_server)
+# https_server = endpoint['https_server']
 
-source_path = "/5/published/publication_170/submitted_data/2015_11_18_cortex/2015_11_18_cortex.json"
+# source_path = "/5/published/publication_170/submitted_data/2015_11_18_cortex/2015_11_18_cortex.json"
 
-# create download link by making path: https_server + source_path
-download_path = op.join(https_server, source_path)
-print(download_path)
-url='https://2a9f4.8443.dn.glob.us/5/published/publication_170/submitted_data/2015_11_18_cortex/2015_11_18_cortex.json'
-wget.download(url=url)
+
+
+# wget.download(url=url)
 # label = "My json transfer"
 #
 # # TransferData() automatically gets a submission_id for once-and-only-once submission
@@ -103,32 +104,6 @@ wget.download(url=url)
 
 # IMPORTANT PAGES: https://docs.globus.org/globus-connect-server-installation-guide/
 # https://docs.globus.org/globus-connect-server-v5-installation-guide/#_creating_a_storage_gateway_using_the_posix_storage_connector
-#
-# # high level interface; provides iterators for list responses
-# reqs = tc.endpoint_get_activation_requirements(endpoint_id)
-# print(reqs['activated'])
-# print(reqs)
-# file = open('tmp.json', 'r')
-#
-# reqs = json.load(file)
-# print(type(reqs))
-
-# ACTIVATION (pause for now)
-# To active an endpoint, clients should get the activation requirements for the endpoint (either explicitly or from
-# the autoactivate result), pick an activation method, and fill in values for the chosen activation method.
-# The requirements for the other methods not being used must be removed before submitting the request.
-# https://docs.globus.org/api/transfer/endpoint_activation/
-# r = tc.endpoint_autoactivate(endpoint_id, if_expires_in=3600)
-#
-# if r["code"] == "AutoActivationFailed":
-#     print("Endpoint requires manual activation, please open "
-#           "the following URL in a browser to activate the "
-#           "endpoint:")
-#     print("https://app.globus.org/file-manager?origin_id=%s"
-#           % endpoint_id)
-    # For python 2.X, use raw_input() instead
-    # input("Press ENTER after activating the endpoint:")
-    # r = tc.endpoint_autoactivate(endpoint_id, if_expires_in=3600)
 
 
 # TRY AND NAVIGATE IN THE ENDPOINT PATH AND RETRIVE DATA
