928d8640c619dc15ece6c33f90e70dfb8a67c05a
gi114 <giulia.ippoliti@mail.mcgill.ca>
2019-10-31 09:51:38 -0400

    temp file

--- lookup_url.py
+++ lookup_url.py
@@ -1,4 +1,7 @@
-
+# TODO: check why these hashes are different, many files as shown in tmp.txt
 lookup_url = {
-    'b35c4252dcc5e1f33999289f75def9ae1eaa70614c6957254e2e9ff4e65bd8f7': '/~/5/published/publication_170/submitted_data/2015_11_18_cortex/mask/mask.mat'
+    'b35c4252dcc5e1f33999289f75def9ae1eaa70614c6957254e2e9ff4e65bd8f7':'/~/5/published/publication_170/submitted_data/2015_11_18_cortex/mask/mask.mat',
+    'ada6e6f11a550ad74b5de919c6ae915db950a3bd7762040f4e49e86cde94e150':'/~/5/published/publication_170/submitted_data/2015_11_18_cortex/processed/2015_11_18_4_filtered0.1to10.mat',
+    '94df7d8ff1554e83ddb0b8fd17b077a76f1fedc7ae2388311d3f718ee6594d20':'/~/5/published/publication_170/submitted_data/2015_11_18_cortex/processed/2015_11_18_5_filtered0.1to10.mat',
+    '5b3bf03ac18cbb87a3c6418ed4e0d57dec7f95c7617c8d0876b2232b3e4185cc':'/~/5/published/publication_170/submitted_data/2015_11_18_cortex/processed/2015_11_18_6_filtered0.1to10.mat'
 }
