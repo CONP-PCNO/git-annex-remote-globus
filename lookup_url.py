4e09bcd4ea2ec128699755082aa1f9ee8f74d1c0
gi114 <giulia.ippoliti@mail.mcgill.ca>
2019-11-01 12:52:48 -0400

    key resolution in look up url

--- lookup_url.py
+++ lookup_url.py
@@ -1,7 +1,9 @@
 # TODO: check why these hashes are different, many files as shown in tmp.txt
 lookup_url = {
-    'b35c4252dcc5e1f33999289f75def9ae1eaa70614c6957254e2e9ff4e65bd8f7':'/~/5/published/publication_170/submitted_data/2015_11_18_cortex/mask/mask.mat',
-    'ada6e6f11a550ad74b5de919c6ae915db950a3bd7762040f4e49e86cde94e150':'/~/5/published/publication_170/submitted_data/2015_11_18_cortex/processed/2015_11_18_4_filtered0.1to10.mat',
-    '94df7d8ff1554e83ddb0b8fd17b077a76f1fedc7ae2388311d3f718ee6594d20':'/~/5/published/publication_170/submitted_data/2015_11_18_cortex/processed/2015_11_18_5_filtered0.1to10.mat',
-    '5b3bf03ac18cbb87a3c6418ed4e0d57dec7f95c7617c8d0876b2232b3e4185cc':'/~/5/published/publication_170/submitted_data/2015_11_18_cortex/processed/2015_11_18_6_filtered0.1to10.mat'
+    '1e5e0b0c5896d16ac14170c8f546d4e1':'/~/5/published/publication_170/submitted_data/2015_11_18_cortex/mask/mask.mat',
+    '4b9fbbb365580b5913b2452531f8d844':'/~/5/published/publication_170/submitted_data/2015_11_18_cortex/processed/2015_11_18_4_filtered0.1to10.mat',
+    '188f948194f044dab61a00ab47ec8e84':'/~/5/published/publication_170/submitted_data/2015_11_18_cortex/processed/2015_11_18_5_filtered0.1to10.mat',
+    '48d272325adaf43bcccf6d7556dbab18':'/~/5/published/publication_170/submitted_data/2015_11_18_cortex/processed/2015_11_18_6_filtered0.1to10.mat',
+    'b641e5adc4bdca1488cfd055cfb1c38f':'/~/5/published/publication_170/submitted_data/2015_11_18_cortex/spike train/2015-11-18-4/c1.txt',
+    '39dfb842ba8d8ca5591f992169374877':'/~/5/published/publication_170/submitted_data/2015_11_18_cortex/2015_11_18_cortex.json'
 }
