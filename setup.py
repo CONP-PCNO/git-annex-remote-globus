067cd530169dfe189a9f8474ff8eaf88d0b400f7
gi114 <giulia.ippoliti@mail.mcgill.ca>
2019-11-28 16:16:30 -0500

    started setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='git-annex-remote-globus',
+    version='0.1',
+    author='Giulia Ippoliti',
+    author_email='giuly.ppoliti@gmail.com',
+    scripts=['git-annex-remote-globus'],
+    description='git annex special remote for Globus',
+    long_description=open('README.md').read()
+)
