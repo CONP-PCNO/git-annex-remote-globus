9192288b5bc3c4b9c1c44994481dc860409c36a6
gi114 <giulia.ippoliti@mail.mcgill.ca>
2019-11-29 14:31:46 -0500

    setup additions

--- setup.py
+++ setup.py
@@ -6,8 +6,22 @@ setup(
     author='Giulia Ippoliti',
     author_email='giuly.ippoliti@gmail.com',
     scripts=['git-annex-remote-globus'],
+    keywords='git-annex remote globus',
     url='https://github.com/CONP-PCNO/git-annex-remote-globus',
     description='git annex special remote for Globus',
     long_description=open('README.md').read(),
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
+    install_requires=['annexremote==1.3.1'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+
+        'Intended Audience :: Developers',
+
+        'Topic :: Software Development :: Libraries',
+
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
 )
