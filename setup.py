f281155d5223645bd78d7b9491bc60bb1da5023a
gi114 <giulia.ippoliti@mail.mcgill.ca>
2019-11-29 12:57:42 -0500

    added self settings

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     name='git-annex-remote-globus',
     version='0.1',
     author='Giulia Ippoliti',
-    author_email='giuly.ppoliti@gmail.com',
+    author_email='giuly.ippoliti@gmail.com',
     scripts=['git-annex-remote-globus'],
     url='https://github.com/CONP-PCNO/git-annex-remote-globus',
     description='git annex special remote for Globus',
-    long_description=open('README.md').read()
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
 )
