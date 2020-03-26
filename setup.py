8c8664ef0c2c1b1f7326bcfa4da4528539d4312d
gi114 <giulia.ippoliti@mail.mcgill.ca>
2020-03-26 11:52:24 -0400

    setup version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='git-annex-remote-globus',
-    version='1.0',
+    version='1.1',
     author='Giulia Ippoliti',
     author_email='giuly.ippoliti@gmail.com',
     scripts=['git-annex-remote-globus'],
